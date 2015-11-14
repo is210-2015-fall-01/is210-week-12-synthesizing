8437316dcce5d9c9966e15e05f89ba9acc87b741
Alex Doler <aedoler@gmail.com>
2015-11-14 15:48:17 +0000

    .

--- task_03.py
+++ task_03.py
@@ -2,12 +2,24 @@
 # -*- coding: utf-8 -*-
 """Task 03 module"""
 
+
 def exception_test(arg1, arg2, arg3):
+    """Doctring
+    Args:
+        arg1:
+        arg2:
+        arg3:
+    Returns:
+        True or False depending on if error caught
+    Examples:
+        >>>exception_test('Hello', 1, 1)
+        True
     caught = False
+    """
+    caught = 0
     try:
         arg1[arg2].index(arg3)
     except (LookupError, TypeError):
         caught = True
 
     return caught
-    
