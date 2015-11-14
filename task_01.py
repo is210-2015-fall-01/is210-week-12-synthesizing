4b4abee4d9371811ef38bc253b3b946d8836d540
Saulat Majid <solyraja@hotmail.com>
2015-11-14 15:13:05 -0500

    Submitting work.

---
+++ task_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creating custom exception classes"""
+
+
+class BaseError(Exception):
+    """A class called BaseError with an argument, Exception"""
+    pass
+    
+class StringError(BaseError, TypeError):
+    """A class, StringError, subclassing BaseError"""
+    pass
+
+class NumberError(BaseError, TypeError):
+    """A class, NumberError, subclassing BaseErrror"""
+    pass
+
+class NonZero(NumberError):
+    """A class, NonZero, subclassing NumberError"""
+    pass
